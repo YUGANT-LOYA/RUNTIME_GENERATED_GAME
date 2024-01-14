cdd5ad631abc5fdd5cf2b7b0d367dc53fff4edc0
YUGANT <yugantloya@gmail.com>
2024-01-14 17:54:17 +0530

    Maze Saving and Loading System Completed

--- Assets/RUNTIME_GENERATED_GAME/DFS/Scripts/LoadSaveMazeSystem.cs
+++ Assets/RUNTIME_GENERATED_GAME/DFS/Scripts/LoadSaveMazeSystem.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,10 +17,11 @@ namespace YugantLoyaLibrary.MazeGenerator.DFS
             public Vector2Int cellId;
             public string cellWallInfo;
         }
-        
+
         public Button saveMazeButton;
+        private MazeGenerator _mazeGenerator;
         public List<MazeCell> mazeCellList = new List<MazeCell>();
-        public MazeDataStruct mazeDataStruct;
+        public List<MazeDataStruct> mazeDataStructList;
 
         private void OnEnable()
         {
@@ -33,18 +35,63 @@ namespace YugantLoyaLibrary.MazeGenerator.DFS
             GameEventHandler.OnMazeCreationCompleteEvent -= ActivateSaveSystem;
         }
 
-        public void SaveIntoJson()
+        private void Awake()
+        {
+            _mazeGenerator = GetComponent<MazeGenerator>();
+            saveMazeButton.onClick.AddListener(SaveIntoJson);
+        }
+
+        private void SaveIntoJson()
         {
             mazeCellList.Clear();
-            mazeCellList = GetComponent<MazeGenerator>().GetMazeCellList();
+            mazeCellList = _mazeGenerator.GetMazeCellList();
             StringBuilder strBuilder = new StringBuilder();
-            foreach (MazeCell cell in mazeCellList)
+
+            // Specify the file path
+            string filePath = Path.Combine("Assets/RUNTIME_GENERATED_GAME/DFS/MazeJson",
+                $"Maze_{_mazeGenerator.mazeNum}.json");
+
+            //If File exists 
+            if (File.Exists(filePath))
             {
-                strBuilder.Clear();
-                MazeDataStruct mazeData = new MazeDataStruct();
-                mazeData.cellId = cell.mazeCellId;
-                strBuilder.Append(cell.GetCellDataForJsonSaving());
-                mazeData.cellWallInfo = strBuilder.ToString();
+                _mazeGenerator.mazeNum++;
+                Debug.Log($"File Already Exists , so changing File Name to  Maze_{_mazeGenerator.mazeNum}.json");
+                SaveIntoJson();
+                return;
+            }
+            else
+            {
+                //Prints the gridSize [x,y]
+                //Prints Source and Destination Location [S,D]
+                //Prints all the cell internal Data Info [XY,WallInfo in terms of 0 and 1]
+
+                strBuilder.AppendLine($"{_mazeGenerator.mazeWidth},{_mazeGenerator.mazeDepth}");
+
+                //if(_mazeGenerator.isPlayerLocationAssigned)
+                strBuilder.AppendLine();
+
+                foreach (MazeCell cell in mazeCellList)
+                {
+                    MazeDataStruct mazeData = new MazeDataStruct
+                    {
+                        cellId = cell.mazeCellId
+                    };
+
+                    strBuilder.Append($"{cell.mazeCellId.x}{cell.mazeCellId.y},");
+                    strBuilder.Append(cell.GetCellDataForJsonSaving());
+                    mazeData.cellWallInfo = strBuilder.ToString();
+                    mazeDataStructList.Add(mazeData);
+                    strBuilder.AppendLine();
+                }
+
+                // Write the JSON data to the file
+                File.WriteAllText(filePath, strBuilder.ToString());
+
+#if UNITY_EDITOR
+                // Refresh the asset database to make Unity aware of the new file
+                UnityEditor.AssetDatabase.Refresh();
+#endif
+                Debug.Log("JSON file created at: " + filePath);
             }
         }
 
@@ -52,7 +99,7 @@ namespace YugantLoyaLibrary.MazeGenerator.DFS
         {
             saveMazeButton.interactable = true;
         }
-        
+
         void DeactivateSaveSystem()
         {
             saveMazeButton.interactable = false;
--- Assets/RUNTIME_GENERATED_GAME/DFS/Scripts/MazeGenerator.cs
+++ Assets/RUNTIME_GENERATED_GAME/DFS/Scripts/MazeGenerator.cs
@@ -23,15 +23,23 @@ namespace YugantLoyaLibrary.MazeGenerator.DFS
         }
 
         public bool isMazeCompleted;
+        public int mazeNum = 1;
+        public bool boolLoadingMazeFile = false;
+        [ShowIf("boolLoadingMazeFile")] public TextAsset mazeJsonFile;
         public List<ShapeStruct> shapeStructList;
         [SerializeField] private Transform mazeCellContainer;
         [SerializeField] private MazeShape currMazeShape;
         [SerializeField] private float camYOffset = 2f, timeToFindNextCell = 0.02f;
-        [SerializeField] private int mazeWidth = 25, mazeDepth = 25;
+        public int mazeWidth = 25, mazeDepth = 25;
         public bool shouldStartRandomly = true;
-        [HideIf("shouldStartRandomly")]
-        [SerializeField] Vector2Int startingMazeCell = Vector2Int.zero;
+
+        [HideIf("shouldStartRandomly")] [SerializeField]
+        Vector2Int startingMazeCell = Vector2Int.zero;
+
+        private Camera _cam;
         private MazeCell[,] _mazeGrid;
+        public bool isPlayerLocationAssigned, isTargetLocationAssigned;
+        public Vector2Int playerLocation, targetLocation;
         public List<MazeCell> totalMazeCellList;
 
         private void OnEnable()
@@ -46,30 +54,52 @@ namespace YugantLoyaLibrary.MazeGenerator.DFS
         {
             GameEventHandler.OnGridCreationCompleteEvent -= GridCreationCompleted;
             GameEventHandler.OnMazeCreationStartEvent -= MazeCreationStarted;
-            GameEventHandler.OnMazeCreationCompleteEvent-= MazeCreationCompleted;
+            GameEventHandler.OnMazeCreationCompleteEvent -= MazeCreationCompleted;
+        }
+
+        private void Awake()
+        {
+            _cam = Camera.main;
         }
 
         private void Start()
         {
             totalMazeCellList = new List<MazeCell>();
-            StartCoroutine(StartGeneratingMaze());
+            //Only runs when we need to create new maze generation
+            if (!boolLoadingMazeFile)
+            {
+                StartCoroutine(StartGeneratingMaze());
+            }
+            else
+            {
+                //This will load the file of mazeJsonFile and create maze using it.
+
+                if (mazeJsonFile != null)
+                {
+                    LoadMazeDataUsingJson();
+                }
+                else
+                {
+                    Debug.LogError("There is No File attached to mazeJsonFile to already created maze !");
+                }
+            }
         }
 
         private void MazeCreationStarted()
         {
             isMazeCompleted = false;
         }
-        
+
         public Transform GetMazeCellContainer()
         {
             return mazeCellContainer;
         }
-        
+
         public List<MazeCell> GetMazeCellList()
         {
             return new List<MazeCell>(totalMazeCellList);
         }
-        
+
         //Access the main Prefab
         private MazeCell GetShapeScriptFromMazeShape(MazeShape shapeEnum)
         {
@@ -96,7 +126,7 @@ namespace YugantLoyaLibrary.MazeGenerator.DFS
             StopAllCoroutines();
             _mazeGrid = new MazeCell[0, 0];
             totalMazeCellList.Clear();
-            
+
             for (int i = mazeCellContainer.childCount - 1; i >= 0; i--)
             {
                 Destroy(mazeCellContainer.GetChild(i).gameObject);
@@ -107,8 +137,6 @@ namespace YugantLoyaLibrary.MazeGenerator.DFS
 
         IEnumerator StartGeneratingMaze()
         {
-            _mazeGrid = new MazeCell[mazeWidth, mazeDepth];
-
             CreateGrid();
 
             GameEventHandler.MazeCreationStarted();
@@ -129,7 +157,7 @@ namespace YugantLoyaLibrary.MazeGenerator.DFS
                 int randomY = UnityEngine.Random.Range(0, mazeDepth);
 
                 startingMazeCell = new Vector2Int(randomX, randomY);
-                
+
                 if (startingMazeCell.x - 1 >= 0)
                 {
                     prevCell = _mazeGrid[startingMazeCell.x - 1, startingMazeCell.y];
@@ -138,9 +166,11 @@ namespace YugantLoyaLibrary.MazeGenerator.DFS
 
             yield return GenerateMaze(prevCell, _mazeGrid[startingMazeCell.x, startingMazeCell.y]);
         }
-        
+
         private void CreateGrid()
         {
+            _mazeGrid = new MazeCell[mazeWidth, mazeDepth];
+            
             MazeCell currMazeScript = GetShapeScriptFromMazeShape(currMazeShape);
 
             for (int x = 0; x < mazeWidth; x++)
@@ -151,12 +181,13 @@ namespace YugantLoyaLibrary.MazeGenerator.DFS
                     _mazeGrid[x, z] = Instantiate(currMazeScript, new Vector3(localScale.x * x, 0, localScale.z * z),
                         Quaternion.identity,
                         mazeCellContainer);
+                    _mazeGrid[x,z].transform.SetAsLastSibling();
                     _mazeGrid[x, z].mazeCellId = new Vector2Int(x, z);
                     totalMazeCellList.Add(_mazeGrid[x, z]);
                     currMazeScript.gameObject.name = $"Cell_{x}_{z}";
                 }
             }
-            
+
             GameEventHandler.GridCreationCompleted();
         }
 
@@ -177,7 +208,7 @@ namespace YugantLoyaLibrary.MazeGenerator.DFS
 
             return true;
         }
-        
+
         IEnumerator GenerateMaze(MazeCell prevCell, MazeCell currCell)
         {
             currCell.Visited();
@@ -197,7 +228,7 @@ namespace YugantLoyaLibrary.MazeGenerator.DFS
                 }
             } while (nextCell != null);
 
-            
+
             if (IsAllCellVisited() && !isMazeCompleted)
             {
                 //Calls When The Whole Maze is Ready
@@ -312,10 +343,79 @@ namespace YugantLoyaLibrary.MazeGenerator.DFS
 
             int max = mazeDepth > mazeWidth ? mazeDepth : mazeWidth;
 
-            var childCount = mazeCellContainer.childCount;
+            int childCount = mazeCellContainer.childCount;
+            
             Camera.main.transform.position =
                 new Vector3(camPos.x / childCount, max * 2 + camYOffset, camPos.z / childCount);
         }
 
+        private void LoadMazeDataUsingJson()
+        {
+            TextAsset mazeFile = mazeJsonFile;
+            string[] data = mazeFile.text.Split('\n');
+            
+            //First Line is of Grid Size
+            string[] gridSizeLine = data[0].Split(',');
+            mazeWidth = int.Parse(gridSizeLine[0]);
+            mazeDepth = int.Parse(gridSizeLine[1]);
+            
+            //Second Line is for reading the Source and Destination of Player.
+            
+            
+            //Creating Grids
+            CreateGrid();
+
+            int dataIndex = 2;
+            
+            for (int i = 0; i < totalMazeCellList.Count; i++)
+            {
+                MazeCell cell = totalMazeCellList[i];
+                
+                //This is the data of each cell of maze about their wall and other data.
+                string[] cellDataStr = data[dataIndex].Split(',');
+
+                //Vector2Int cellId = new Vector2Int(cellDataStr[0][0], cellDataStr[0][1]);
+                
+                //cellDataStr[1] is the data of walls
+                string wallDetailStr = cellDataStr[1];
+                Debug.Log("Wall Detail : " + wallDetailStr);
+                cell.LeftWallStatus(int.Parse(wallDetailStr[0].ToString()) == 1);
+                cell.RightWallStatus(int.Parse(wallDetailStr[1].ToString()) == 1);
+                cell.FrontWallStatus(int.Parse(wallDetailStr[2].ToString()) == 1);
+                cell.BackWallStatus(int.Parse(wallDetailStr[3].ToString()) == 1);
+                cell.Visited();
+                dataIndex++;
+            }
+            
+            
+            
+            // for (int i = 0; i < data.Length; i++)
+            // {
+            //     string line = data[i];
+            //
+            //     if (i == 0)
+            //     {
+            //         //First Line is of Grid Size
+            //         mazeWidth = int.Parse(line[0].ToString());
+            //         mazeDepth = int.Parse(line[1].ToString());
+            //     }
+            //     else if (i == 1)
+            //     {
+            //         //Second Line is for reading the Source and Destination of Player.
+            //         
+            //     }
+            //     else
+            //     {
+            //         //This is the data of each cell of maze about their wall and other data.
+            //         string[] cellDataStr = line.Split(',');
+            //
+            //         Vector2Int cellId = new Vector2Int(cellDataStr[0][0], cellDataStr[0][1]);
+            //         for (var index = 0; index < cellDataStr.Length; index++)
+            //         {
+            //             var str = cellDataStr[index];
+            //         }
+            //     }
+            // }
+        }
     }
 }
